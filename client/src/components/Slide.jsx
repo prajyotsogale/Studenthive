22b9ceab1bf2ae1b28ce8bd498ff9931b7f293ce
prajyotsogale <psogale212@gmail.com>
2024-10-26 11:23:12 +0530

    slide

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,19 @@
+import "../styles/Slide.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Slide = () => {
+  return (
+    <div className="slide container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="mx-auto">
+          Welcome to StudentHive! Wherever your journey takes you, find a cozy home nearby. 
+          Create unforgettable moments while you study and thrive!
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
