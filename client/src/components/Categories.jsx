77fd76f472e850e3789d584a2a96a25ca3757be7
prajyotsogale <psogale212@gmail.com>
2024-10-27 18:43:52 +0530

    slide

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { categories } from "../data";
+import "../styles/Categories.scss"
+import { Link } from "react-router-dom";
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
-}
+    <div className="categories">
+      <h1>Explore Top Categories</h1>
+      <p>
+        Explore our wide range of apartments.
+      </p>
 
-export default Categories
+      <div className="categories_list">
+        {categories?.slice(1, 7).map((category, index) => (
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
+              <img src={category.img} alt={category.label} />
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
+import Categories from "../components/Categories"
 import Navbar from "../components/Navbar"
 import Slide from "../components/Slide"
+
 
 
 const HomePage = () => {
@@ -7,6 +9,7 @@ const HomePage = () => {
     <>
       <Navbar />
       <Slide />
+      <Categories/>
     </>
   )
 }
