38d64ab16fa0867959a21df711676d275fc59e08
prajyotsogale <psogale212@gmail.com>
2025-02-20 16:41:33 +0530

    onlinerender

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://studenthive.onrender.com/properties?category=${selectedCategory}`
+          : "https://studenthive.onrender.com/properties",
         {
           method: "GET",
         }
